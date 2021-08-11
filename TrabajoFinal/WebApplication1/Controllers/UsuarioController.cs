b31c6b7c99e88bb9a2c629ba5af920b011a808cf
zzsantoszz <santsmax12@gmail.com>
2021-08-10 23:51:37 -0500

    cambios

---
+++ TrabajoFinal/WebApplication1/Controllers/UsuarioController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DAO.Modelo;
+using DAO;
+
+namespace WebApplication1.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        private connBD_EF conn = new connBD_EF();
+        // GET: Usuario
+        public ActionResult Index()
+        {
+            
+            return View(conn.tb_usuario.ToList());
+        }
+    }
+}
